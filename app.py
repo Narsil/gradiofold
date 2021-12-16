b7b5e1a486b92daf6463d1327290451c7d4c9cb7
Nicolas Patry <patry.nicolas@protonmail.com>
2021-12-16 16:39:05 +0100

    Initial commit
    Tmp.
    
    Tmp
    
    Iframe ?
    
    Tmp
    
    Tmp.
    
    Changing filename name.
    
    Fix.
    
    Prod build.
    
    Remove server port.

---
+++ app.py
@@ -0,0 +1,31 @@
+import gradio as gr
+from pathlib import Path
+import tempfile
+
+
+def molstar(output_name):
+    structure_url = f"https://nodata.dev/gradiofold/outputs/{output_name}"
+    return f'<iframe style="height: 50rem; width:100%" src="https://nodata.dev/gradiofold/molstar.html?structure-url={structure_url}"/>'
+
+
+def main(dropdown):
+    filename = PRECALCULATED_MOLECULES[dropdown]
+    return molstar(filename)
+
+
+PRECALCULATED_MOLECULES = {"Covid spike (alpha)": "covid-alpha.pdb"}
+
+
+file_input = gr.inputs.File(
+    file_count="single",
+    type="file",
+    label="Send a Fasta file representing the protein",
+    optional=False,
+)
+dropdown_input = gr.inputs.Dropdown(
+    list(PRECALCULATED_MOLECULES.keys()) + ["custom fasta file"]
+)
+inputs = [dropdown_input]
+iface = gr.Interface(fn=main, inputs=inputs, outputs="html")
+if __name__ == "__main__":
+    iface.launch()
